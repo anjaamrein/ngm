1377a2a2c172c3ef72ee826593a35397f9da4cd2
Guillaume Beraudo <guillaume.beraudo@camptocamp.com>
2021-03-23 15:12:17 +0100

    Simplify deploy script

--- scripts/deploy_to_prod.sh
+++ scripts/deploy_to_prod.sh
@@ -8,11 +8,9 @@ fi
 
 PROD_TAG="prod_`date '+r_%Y_%m_%d_%Hh%M'`"
 git fetch
-git checkout prod
-git reset --hard origin/prod
-git pull origin $VERSION
+git checkout $VERSION
 make dist
 scripts/deploy_to_s3.sh prod
 git tag $PROD_TAG -m $PROD_TAG
 git push origin $PROD_TAG
-git push origin prod
+git push -f origin prod
